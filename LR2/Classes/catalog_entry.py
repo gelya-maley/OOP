c99819abb93e7a53250906978dc93b406f04a2f1
Angelina Malei <maley.gelya@mail.ru>
2026-02-26 15:27:08 +0300

    branch LR1

--- LR2/Classes/catalog_entry.py
+++
@@ -1,7 +0,0 @@
-class CatalogEntry:
-    def __init__(self, book, shelf):
-        self.__book = book
-        self.__shelf = shelf
-
-    def get_location(self):
-        return f"Книга '{self.__book.get_title()}' на полке {self.__shelf}"
--- LR2/Classes/genre.py
+++
@@ -1,6 +0,0 @@
-class Genre:
-    def __init__(self, name):
-        self.__name = name
-
-    def get_name(self):
-        return self.__name
